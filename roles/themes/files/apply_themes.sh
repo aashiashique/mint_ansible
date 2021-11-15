c54e8f2ca838739cdc328f795813d1c86302e777
AaShi <aashi_ashique@yahoo.com>
2021-11-15 23:44:48 +0530

    added theme script

---
+++ roles/themes/files/apply_themes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y-Dark"
+gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Dark-Grey"
+gsettings set org.cinnamon.theme name "CBlue"
+gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y-Dark-Grey"
+gsettings set org.cinnamon.desktop.interface cursor-theme "DMZ-Black"
